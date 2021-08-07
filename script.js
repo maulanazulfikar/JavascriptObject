b76fb477e01e6f9438670e7e75b221ae6ddeddf0
maulanazulfikar <ymci88@gmail.com>
2021-08-08 03:29:20 +0700

    pertama

---
+++ script.js
@@ -0,0 +1,41 @@
+//no.1
+let Gorilla = {
+    Nama: 'Gorilla',
+    Kaki: 2,
+    Spesies: 'Primata',
+    Warna: ['Hitam', 'Cokelat', 'Albino'],
+    Makanan: ['Buah', 'Sayur', 'Daging'],
+    Suara: 'Huhuhaha',
+    Keahlian: () => {
+        return "Berkoloni";
+    }
+  };
+  
+  console.log("Nama: ", Gorilla.Nama);
+  console.log("Jumlah Kaki: ", Gorilla.Kaki);
+  console.log("Spesies: ", Gorilla.Spesies);
+  console.log("Warna: ", Gorilla.Warna[0], ",", Gorilla.Warna[1], ",", Gorilla.Warna[2]);
+  console.log("Makanan: ", Gorilla.Makanan[0], ",", Gorilla.Makanan[1], ",", Gorilla.Makanan[2]);
+  console.log("Suara: ", Gorilla.Suara);
+  console.log("Keahlian: ", Gorilla.Keahlian());
+  
+  //no.2
+  const data = 
+[
+  {
+    name: 'Alpha',
+    class: 'Dragon',
+    club: ['Bola', ' Bulutangkis.'],
+  },
+  {
+    name: 'Beta',
+    class: 'Lizard',
+    club: ['Membaca', ' Bulutangkis.'],
+  },
+ ];
+ console.log(data[0].name +  " ada di kelas " + data[0].class + " dia mengikuti " + data[0].club);
+ console.log(data[1].name +  " ada di kelas " + data[1].class + " dia mengikuti " + data[1].club);
+
+ //no.3
+
+ //no.4
